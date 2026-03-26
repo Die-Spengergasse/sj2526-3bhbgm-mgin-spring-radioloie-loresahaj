fb103a6a9d2035e5bfb524069077b4c7b21bcffa
loresa <haj230376@spengergasse.at>
2026-03-26 16:02:02 +0100

    speichern funktioniert noch nicht

--- src/main/java/at/spengergasse/spring_thymeleaf/entities/Patient.java
+++ src/main/java/at/spengergasse/spring_thymeleaf/entities/Patient.java
@@ -13,15 +13,50 @@ public class Patient {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private String name;
+    private String socialSecurityNumber;
+    private String firstName;
+    private String lastName;
+    private String gender;
     private LocalDate birth;
 
-    public String getName() {
-        return name;
+    public int getId() {
+        return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 
     public LocalDate getBirth() {
